41cec3916af782c7708ae761fff8509af3899579
tresna <tresna312@gmail.com>
2023-09-08 14:10:42 +0600

    m

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class PostController extends Controller
 {
-    //
+    public function ShowAllPost(){
+        $ShowAllPost = Http::get('https://jsonplaceholder.typicode.com/posts');
+        return $ShowAllPost->json();
+    }
 }
