1ca895442c1681ab96338824692ef0a697e92216
tresna <tresna312@gmail.com>
2023-09-13 10:52:36 +0600

    com

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,4 @@
 <x-header data='abc'/>
-<x-header data ='123'/>
-<x-header data = 'learner'/>
+<x-header data='123'/>
+<x-header data='learner'/>
 <h1>this ia a about page</h1>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,8 +1,6 @@
 
-
-
 <x-header data='abc'/>
-<x-header data ='123'/>
+<x-header data =123/>
 <x-header data = 'learner'/>
 <h1>this ia a about page</h1>
 <h1>this is a contact page</h1>
