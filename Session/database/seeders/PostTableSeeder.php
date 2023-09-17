9e428b0e2dfda75e2b883121f36135f96164ce59
tresna <tresna312@gmail.com>
2023-09-17 09:31:43 +0600

    seeser

--- Session/database/seeders/PostTableSeeder.php
+++ Session/database/seeders/PostTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
 
 class PostTableSeeder extends Seeder
 {
@@ -12,6 +13,9 @@ class PostTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $posts = [
+            ['id'=>'1','title'=>'post one','description'=>'des one'],
+        ];
+        Post::insert($posts);
     }
 }
