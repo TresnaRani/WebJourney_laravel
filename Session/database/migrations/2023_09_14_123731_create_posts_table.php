1d31740f6e5d8120afd9550afce42b96bfa9c813
tresna <tresna312@gmail.com>
2023-09-17 09:11:22 +0600

    seeser

--- Session/database/migrations/2023_09_14_123731_create_posts_table.php
+++ Session/database/migrations/2023_09_14_123731_create_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->tinyInteger('status')->default('1');
             $table->string('description')->nullable();
             $table->timestamps();
         });
